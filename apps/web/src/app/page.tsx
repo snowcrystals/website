31eb922309a6709ba1cb312d35c51669c8b4a283
Daan Klarenbeek <github@ijskoud.dev>
2023-05-04 11:37:49 +0200

    feat: make responsive

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,7 +7,7 @@ const Page: React.FC = () => {
 			<LandingBackground />
 			<main className="h-screen w-screen relative z-0">
 				<div className="flex items-center justify-center w-full h-full">
-					<h1 className="text-12 font-bold w-1/2 text-center">
+					<h1 className="text-12 font-bold w-1/2 text-center max-xl:w-3/4 max-sm:w-full">
 						Documentation for npm packages by <span className="bg-primary py-1 px-2 rounded-lg">ijsKoud</span>
 					</h1>
 				</div>
--- packages/ui/src/Landing/Background.tsx
+++ packages/ui/src/Landing/Background.tsx
@@ -5,12 +5,17 @@ import React from "react";
 export const LandingBackground: React.FC = () => {
 	return (
 		<>
-			<div aria-hidden className="fixed -z-1 top-0 left-0 -translate-x-96 rotate-12 scale-110">
+			{/* Left Top Snowflake */}
+			<div aria-hidden className="fixed -z-1 top-0 left-0 -translate-x-96 rotate-12 scale-110 max-xl:-translate-y-80">
 				<SnowFlake />
 			</div>
-			<div aria-hidden className="fixed -z-1 top-0 right-0 -translate-y-96 -translate-x-28 scale-75 rotate-[50deg]">
+
+			{/* Top Middle Snowflake */}
+			<div aria-hidden className="fixed -z-1 top-0 right-0 -translate-y-96 -translate-x-28 scale-75 rotate-[50deg] max-xl:hidden">
 				<SnowFlake />
 			</div>
+
+			{/* Right Bottom Snowflake */}
 			<div aria-hidden className="fixed -z-1 bottom-0 right-0 translate-y-96 translate-x-72 -rotate-45 scale-90">
 				<SnowFlake />
 			</div>
