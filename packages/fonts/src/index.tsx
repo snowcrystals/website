332ce1cb43f7affb04ed8d32e150806f3a5a87bd
Daan Klarenbeek <github@ijskoud.dev>
2023-05-08 11:31:05 +0200

    feat(Fonts): add inter font

--- packages/fonts/src/index.tsx
+++ packages/fonts/src/index.tsx
@@ -1,3 +1,4 @@
-import { Poppins } from "@next/font/google";
+import { Poppins, Inter } from "@next/font/google";
 
-export const PoppinsFont = Poppins({ display: "swap", subsets: ["latin"], weight: ["300", "400", "500", "600", "700", "800"] });
+export const PoppinsFont = Poppins({ display: "swap", subsets: ["latin"], weight: ["300", "400", "500", "600", "700", "800"], preload: true });
+export const InterFont = Inter({ display: "swap", subsets: ["latin"], weight: ["300", "400", "500", "600", "700", "800"], preload: true });
