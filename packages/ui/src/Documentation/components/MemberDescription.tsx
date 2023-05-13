c54b2b0e06c05e3fa84e068800d5a484f350bec2
Daan Klarenbeek <github@ijskoud.dev>
2023-05-14 00:33:33 +0200

    feat: add markdown to all descriptions

--- packages/ui/src/Documentation/components/MemberDescription.tsx
+++ packages/ui/src/Documentation/components/MemberDescription.tsx
@@ -1,4 +1,5 @@
 import { DocumentTextIcon } from "@heroicons/react/24/outline";
+import { ReadmeMarkdown } from "@website/markdown";
 import React from "react";
 import type { CommentParser } from "typedoc-json-parser";
 
@@ -12,11 +13,15 @@ export const MemberDescription: React.FC<Props> = ({ comment }) => {
 			<h2 className="flex items-center gap-2 text-6 font-medium">
 				<DocumentTextIcon className="h-6" /> Description
 			</h2>
-			<p>{comment.description}</p>
+			<div className="[&>div.markdown]:!font-mono [&>div.markdown]:px-0">
+				<ReadmeMarkdown content={comment.description} fullName="" version="" />
+			</div>
 			{comment.blockTags.map((tag) => (
 				<div key={tag.name} className="mt-2">
 					<h3 className="font-semibold text-5">{tag.name}</h3>
-					<p>{tag.text}</p>
+					<div className="[&>div.markdown]:!font-mono [&>div.markdown]:px-0">
+						<ReadmeMarkdown content={tag.text} fullName="" version="" />
+					</div>
 				</div>
 			))}
 		</section>
--- packages/ui/src/Documentation/components/index.ts
+++ packages/ui/src/Documentation/components/index.ts
@@ -2,3 +2,4 @@ export * from "./MemberDescription";
 export * from "./MemberTitle";
 export * from "./MemberParameters";
 export * from "./MemberTypeParameters";
+export * from "./MemberProperties";
