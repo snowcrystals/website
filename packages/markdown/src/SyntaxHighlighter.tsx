ef2547c46117def653d4278888683672ffeeb362
Daan Klarenbeek <github@ijskoud.dev>
2023-05-13 14:21:01 +0200

    fix(SyntaxHighlighter): incorrect props

--- packages/markdown/src/SyntaxHighlighter.tsx
+++ packages/markdown/src/SyntaxHighlighter.tsx
@@ -1,15 +1,16 @@
-import { Code } from "bright";
+import { Code, type BrightProps } from "bright";
+import type React from "react";
 
-export const SyntaxHighlighter = (props: typeof Code) => {
+export const SyntaxHighlighter: React.FC<Partial<BrightProps>> = (props) => {
 	return (
 		<>
 			<div className="hidden dark:block">
 				{/* @ts-expect-error async component */}
-				<Code codeClassName="font-mono" lang={props.lang ?? "typescript"} {...props} theme="github-dark-dimmed" />
+				<Code codeClassName="font-mono" lang={props.lang ?? "ts"} {...props} theme="github-dark-dimmed" />
 			</div>
 			<div className="block dark:hidden [&_pre]:border [&_pre]:border-gray-300 [&_pre]:rounded-md">
 				{/* @ts-expect-error async component */}
-				<Code codeClassName="font-mono" lang={props.lang ?? "typescript"} {...props} theme="min-light" />
+				<Code codeClassName="font-mono" lang={props.lang ?? "ts"} {...props} theme="min-light" />
 			</div>
 		</>
 	);
