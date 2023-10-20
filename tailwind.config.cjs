6c4c400e5c88d516b5898a2f49dcf9605dc4de40
Daan Klarenbeek <github@ijskoud.dev>
2023-10-20 16:06:53 +0200

    chore: configure shadcn-ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,42 +1,86 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
 const plugin = require("tailwindcss/plugin");
-
-const hocusPlugin = plugin((api) => {
-	api.addVariant("hocus", ["&:focus", "&:hover"]);
-});
-
-const generateSizes = (count, unit) =>
-	Array(count)
-		.fill(null)
-		.map((_, key) => ({ [key + 1]: `${(key + 1) * 4}${unit || "px"}` }))
-		.reduce((a, b) => ({ ...a, ...b }), {});
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [],
 	darkMode: "class",
 	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px"
+			}
+		},
 		extend: {
-			fontSize: generateSizes(24),
-
-			// Widths
-			width: generateSizes(96, "rem"),
-			maxWidth: generateSizes(96, "rem"),
-			minWidth: generateSizes(96, "rem"),
-
-			// Colors
-			borderColor: {},
+			fontSize: Array(24)
+				.fill(null)
+				.map((_, key) => ({ [key + 1]: [(key + 1) * 4] }))
+				.reduce((a, b) => ({ ...a, ...b }), {}),
 			colors: {
-				primary: "#73A2FE",
-				white: "#fff",
-
-				"markdown-dark": "#333333",
-				"markdown-light": "#d8dee4"
+				dark: "#131313",
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))"
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))"
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))"
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))"
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))"
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))"
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))"
+				}
 			},
-			backgroundColor: {
-				dark: "#1C1C1C",
-				primary: "#73A2FE"
+			borderRadius: {
+				lg: `var(--radius)`,
+				md: `calc(var(--radius) - 2px)`,
+				sm: "calc(var(--radius) - 4px)"
+			},
+			fontFamily: {
+				sans: ["var(--font-sans)", ...fontFamily.sans]
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: 0 },
+					to: { height: "var(--radix-accordion-content-height)" }
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: 0 }
+				}
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out"
 			}
 		}
 	},
-	plugins: [hocusPlugin]
+	plugins: [
+		require("tailwindcss-animate"),
+		plugin((api) => {
+			api.addVariant("hocus", ["&:hover", "&:focus"]);
+		})
+	]
 };
