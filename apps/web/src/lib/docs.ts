f58df2150de7c0d0c6421b67548e43a8f1fc3857
Daan Klarenbeek <github@ijskoud.dev>
2023-10-21 15:35:03 +0200

    feat: add version selector (#88)
    * feat(DocsLib): add getVersions function
    
    * feat(DocsLib): add getVersions function
    
    * feat: add version selector page

--- apps/web/src/lib/docs.ts
+++ apps/web/src/lib/docs.ts
@@ -8,3 +8,22 @@ export async function getPackages() {
 	const entries = await readdir(DOCS_DIRECTORY);
 	return entries.filter((str) => statSync(join(DOCS_DIRECTORY, str)).isDirectory());
 }
+/**
+ * Returns a list of available versions for the provided package
+ * @param pkg The package to check
+ * @returns
+ */
+export async function getVersions(pkg: string): Promise<string[] | null> {
+	try {
+		const entries = await readdir(join(DOCS_DIRECTORY, pkg));
+		const versions = entries
+			.filter((str) => str.endsWith(".json"))
+			.map((str) => str.replace(".json", ""))
+			.filter((str) => str !== "main")
+			.reverse();
+
+		return ["main", ...versions];
+	} catch (err) {
+		return null;
+	}
+}
