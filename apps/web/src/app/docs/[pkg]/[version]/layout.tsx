0ef145c7040ad12a3c438814d57979ba83eff70f
Daan Klarenbeek <github@ijskoud.dev>
2023-10-21 19:43:37 +0200

    feat: add docs home page

---
+++ apps/web/src/app/docs/[pkg]/[version]/layout.tsx
@@ -0,0 +1,41 @@
+import type React from "react";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { getPackageDocumentation } from "@/lib/docs";
+import { NavigationMenu } from "@/components/NavigationMenu";
+
+export interface PackageVersionParams {
+	pkg: string;
+	version: string;
+}
+
+export const generateMetadata = ({ params }: { params: PackageVersionParams }): Metadata => {
+	return {
+		title: `@snowcrystals/${params.pkg}: ${params.version}`
+	};
+};
+
+const Layout = async ({ children, params }: React.PropsWithChildren<{ params: PackageVersionParams }>) => {
+	const project = await getPackageDocumentation(params.pkg, params.version);
+	if (!project) notFound();
+
+	return (
+		<div className="mx-auto">
+			<NavigationMenu version={params.version} repository={params.pkg} />
+			<div className="relative top-2.5 mx-auto max-w-7xl gap-6 lg:max-w-full lg:flex">
+				<div className="lg:sticky lg:top-24 lg:left-2 lg:h-[calc(100vh-112px)]">
+					{/* <SideNavbar
+						project={jsonData}
+						packages={packages}
+						versions={versions}
+						currentPackage={params.pkg}
+						currentVersion={params.version}
+					/> */}
+				</div>
+				<div className="mx-auto max-w-5xl min-w-5 w-full pb-10 pt-6">{children}</div>
+			</div>
+		</div>
+	);
+};
+
+export default Layout;
