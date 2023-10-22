29003494a416f11baa958bcd8f80b3cea1f195c5
Daan Klarenbeek <github@ijskoud.dev>
2023-10-22 16:24:57 +0200

    feat(Markdown): change darkmode theme

--- packages/markdown/src/SyntaxHighlighter.tsx
+++ packages/markdown/src/SyntaxHighlighter.tsx
@@ -6,7 +6,7 @@ export const SyntaxHighlighter: React.FC<Partial<BrightProps>> = (props) => {
 	return (
 		<React.Fragment>
 			<div className="hidden dark:block">
-				<Code codeClassName={jetBrainsMono.className} lang={props.lang ?? "ts"} {...props} theme="github-dark-dimmed" />
+				<Code codeClassName={jetBrainsMono.className} lang={props.lang ?? "ts"} {...props} theme="one-dark-pro" />
 			</div>
 			<div className="block dark:hidden [&_pre]:border [&_pre]:border-gray-300 [&_pre]:rounded-md">
 				<Code codeClassName={jetBrainsMono.className} lang={props.lang ?? "ts"} {...props} theme="min-light" />
