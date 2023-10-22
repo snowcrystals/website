12382559852bd2527b007e6730110e349b19f068
Daan Klarenbeek <github@ijskoud.dev>
2023-10-22 19:59:19 +0200

    feat: add interface documentation

--- apps/web/src/app/docs/[pkg]/[version]/[member]/page.tsx
+++ apps/web/src/app/docs/[pkg]/[version]/[member]/page.tsx
@@ -16,6 +16,7 @@ import {
 import { ClassDocumentation } from "@/components/docs/ClassDocumentation";
 import { VariableDocumentation } from "@/components/docs/VariableDocumentation";
 import { TypeAliasDocumentation } from "@/components/docs/TypeAliasDocumentation";
+import { InterfaceDocumentation } from "@/components/docs/InterfaceDocumentation";
 
 function getComponent(member: SearchResult, params: PackageMemberParams) {
 	function getType(result: any): "classes" | "enums" | "variables" | "typeAliases" | "interfaces" | "functions" | "namespaces" | null {
@@ -42,8 +43,8 @@ function getComponent(member: SearchResult, params: PackageMemberParams) {
 			return <VariableDocumentation member={member as any} />;
 		case "typeAliases":
 			return <TypeAliasDocumentation member={member as any} />;
-		// case "interfaces":
-		// 	return <InterfaceDocumentation member={member as any} params={params} />;
+		case "interfaces":
+			return <InterfaceDocumentation member={member as any} params={params} />;
 		case "classes":
 			return <ClassDocumentation member={member as any} params={params} />;
 		// case "enums":
