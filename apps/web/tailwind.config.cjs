f27448caa79b9a1d1e1b1b83d7da048be6efbfbd
Daan Klarenbeek <github@ijskoud.dev>
2024-03-31 23:37:27 +0200

    fix: packages not found

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -1,12 +1,7 @@
-const { readdirSync } = require("node:fs");
-const { join } = require("node:path");
 const config = require("../../tailwind.config.cjs");
-
-const packages = readdirSync(join("..", "..", "packages"));
-const packagesPaths = packages.map((pkg) => `../../packages/${pkg}/**/*.{js,ts,jsx,tsx}`);
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	...config,
-	content: ["./src/app/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}", ...packagesPaths]
+	content: ["./src/app/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"]
 };
