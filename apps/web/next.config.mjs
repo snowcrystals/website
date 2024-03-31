8afe6ade459ce9d025f4336f277706c622a0384c
Daan Klarenbeek <github@ijskoud.dev>
2024-03-31 23:29:53 +0200

    fix: no packages

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,13 +1,7 @@
-import { readdirSync } from "node:fs";
-import { join } from "node:path";
-
-const transpilePackages = readdirSync(join(process.cwd(), "..", "..", "packages")).map((pkg) => `@website/${pkg}`);
-
 /** @type {import('next').NextConfig} */
 const config = {
 	reactStrictMode: true,
 	images: { domains: ["raw.githubusercontent.com"] },
-	transpilePackages,
 	redirects: () => {
 		/** @type {import('next').Redirect[]} */
 		const redirects = [
